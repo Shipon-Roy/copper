5211209bcb6774166591278d11dedd3d7a3b400f
Shipon-Roy <shiponroy509@gmail.com>
2025-11-19 03:44:42 +0600

    work

--- app/layout.js
+++ app/layout.js
@@ -23,10 +23,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Navbar */}
         <Navber />
 
-        {/* Page Content */}
         <main>{children}</main>
       </body>
     </html>
